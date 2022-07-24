4f50b148b87f4d4396ea4c85a28b11ecdbcb424d
justinrivera47 <justinrivera47@gmail.com>
2022-07-24 18:47:22 -0400

    added seeds

--- data/seeds/01-cars.js
+++ data/seeds/01-cars.js
@@ -1,1 +1,32 @@
 // STRETCH
+const cars = [
+  {
+    vin: "1221212121122",
+    make: "honda",
+    model: "accord",
+    mileage: 12343,
+    transmission: "manual",
+    title: "clean"
+  },
+  {
+    vin: "1221212124321",
+    make: "mercedes",
+    model: "es6",
+    mileage: 12343,
+    transmission: "automatic",
+    title: "salvage"
+  },
+  {
+    vin: "1221212121167",
+    make: "ford",
+    model: "focus",
+    mileage: 12343,
+  },
+]
+
+exports.seeds = function (knex) {
+  return knex('cars')
+  .truncate().then(()=>{
+    return knex('cars').insert(cars)
+  })
+}
