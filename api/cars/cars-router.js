976d7686a031949ba5a968ab2aeddb4b5648ade0
justinrivera47 <justinrivera47@gmail.com>
2022-07-24 20:50:29 -0400

    got the end points wired up

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,15 @@
-// DO YOUR MAGIC
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  res.json('getting all cars')
+})
+router.get('/:id', async (req, res) => {
+  res.json('getting car by id')
+})
+router.post('/', async (req, res) => {
+  res.json('posting new car')
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,13 +1,16 @@
 const express = require("express")
+const carsRouter = require("./cars/cars-router")
 
 const server = express()
 
 server.use(express.json())
 
+server.use('/api/cars', carsRouter)
+
 server.use('*', (req, res, next) => {
   next({
     status: 404, 
-    message: 'not found'})
+    message: 'not found!'})
 })
 
 server.use((err, req, res, next) => {
