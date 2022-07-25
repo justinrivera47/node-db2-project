d62b76814e40f41c587a11f3a87b5692d0fe0de1
justinrivera47 <justinrivera47@gmail.com>
2022-07-25 09:37:40 -0400

    tought time getting the data to render

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,14 +1,20 @@
 const express = require('express');
+const Car = require('./cars-model')
 
 const router = express.Router();
 
-router.get('/', async (req, res) => {
-  res.json('getting all cars')
+router.get('/', async (req, res, next) => {
+  try {
+    const cars = await Car.getAll()
+    res.json(cars)
+  } catch (err) {
+    next(err)
+  }
 })
-router.get('/:id', async (req, res) => {
-  res.json('getting car by id')
+router.get('/:id', async (req, res, next) => {
+  res.json(`getting car by id ${req.params.id}`)
 })
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   res.json('posting new car')
 })
 
